22fa7b5ec599acaa0152bc2a12599d66ab242fef
isbs <t070134@gmail.com>
2022-04-02 23:09:16 +0900

    add #myFunction2()

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 async function myFunction() {
-    const provider = await new ethers.providers.Web3Provider(window.ethereum)
+    const provider = await new ethers.providers.Web3Provider(window.ethereum);
     const blockNumber = await provider.getBlockNumber();
-    console.log(blockNumber);
+    console.log(`blockNumber: ${blockNumber}`);
 }
-myFunction();
+
+async function myFunction2() {
+    const provider = await new ethers.providers.Web3Provider(window.ethereum);
+    const addresses = await ethereum.request({ method: 'eth_requestAccounts' });
+    const signer = await provider.getSigner();
+    console.log(`addresses[0]: ${addresses[0]}`);
+}
+
+window.onload = function() {
+    myFunction();
+    myFunction2();
+}
